a40b59a65da5891f2e156c22c5414bd84291f0b0
boozzee <kekc74rus@gmail.com>
2023-12-20 02:04:33 +0500

    use multiple rect, added destr

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include "SFML/Graphics/RectangleShape.hpp"
 #include "SFML/Window/Event.hpp"
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <iterator>
-#include <ostream>
 #include <string>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window.hpp>
 #include <tuple>
+#include <vector>
 
 class WindowConfig {
 public:
@@ -19,9 +17,10 @@ public:
 
   WindowConfig() {}
   WindowConfig(int height, int width) : height(height), width(width) {}
+  ~WindowConfig();
 };
 
-class Rectangle {
+class RectangleParams {
 public:
   int height = 0;
   int width = 0;
@@ -31,22 +30,20 @@ public:
   std::tuple<int, int, int> rgb;
   std::tuple<float, float> size;
 
-  Rectangle() {}
-  Rectangle(int height, int width, std::string name,
-            std::tuple<float, float> position,
-            std::tuple<float, float> velocity, std::tuple<int, int, int> rgb,
-            std::tuple<float, float> size)
+  RectangleParams() {}
+  RectangleParams(int height, int width, std::string name,
+                  std::tuple<float, float> position,
+                  std::tuple<float, float> velocity,
+                  std::tuple<int, int, int> rgb, std::tuple<float, float> size)
       : height(height), width(width), name(name), position(position),
         velocity(velocity), rgb(rgb), size(size) {}
+  ~RectangleParams();
 };
 
-std::tuple<WindowConfig, Rectangle>
-read_config_file(const std::string filename) {
+void read_config_file(const std::string filename, WindowConfig &window,
+                      std::vector<RectangleParams> &rects) {
   std::fstream fin(filename);
   std::string temp;
-
-  WindowConfig window;
-  Rectangle rectangle;
 
   while (fin >> temp) {
     if (temp == "Window") {
@@ -55,6 +52,7 @@ read_config_file(const std::string filename) {
       continue;
     }
     if (temp == "Rectangle") {
+      RectangleParams rectangle;
       fin >> rectangle.name;
       float x, y, vx, vy;
       fin >> x, fin >> y, fin >> vx, fin >> vy;
@@ -65,58 +63,60 @@ read_config_file(const std::string filename) {
       rectangle.rgb = {r, g, b};
       fin >> x, fin >> y;
       rectangle.size = {x, y};
+      rects.push_back(rectangle);
     }
   }
   fin.close();
-  return std::tuple(window, rectangle);
 }
 
 int main(int argc, char *argv[]) {
   const std::string config_file_path = "config.txt";
-  auto data = read_config_file(config_file_path);
+  WindowConfig window_conf;
+  std::vector<RectangleParams> rects_conf;
+  read_config_file(config_file_path, window_conf, rects_conf);
 
-  WindowConfig window_config = std::get<0>(data);
-  Rectangle rectangle_config = std::get<1>(data);
-
-  sf::RenderWindow window(
-      sf::VideoMode(window_config.height, window_config.width), "SFML window");
+  sf::RenderWindow window(sf::VideoMode(window_conf.height, window_conf.width),
+                          "SFML window");
   window.setFramerateLimit(60);
 
-  sf::RectangleShape rect(
-      std::make_from_tuple<sf::Vector2f>(std::move(rectangle_config.size)));
-  rect.setFillColor(
-      std::make_from_tuple<sf::Color>(std::move(rectangle_config.rgb)));
+  std::vector<sf::RectangleShape> rects;
 
-  float Xv = 1.f, Vy = 0.5f;
+  for (auto &elem : rects_conf) {
+    sf::RectangleShape shape(
+        std::make_from_tuple<sf::Vector2f>(std::move(elem.size)));
+    shape.setFillColor(std::make_from_tuple<sf::Color>(std::move(elem.rgb)));
+    rects.push_back(shape);
+  }
 
   while (window.isOpen()) {
-    int WINDOW_W = window.getSize().x;
-    int WINDOW_H = window.getSize().y;
+    const int WINDOW_W = window.getSize().x;
+    const int WINDOW_H = window.getSize().y;
 
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
-
     window.clear();
-    // auto vec = std::make_from_tuple<sf::Vector2f>(
-    //     std::move(rectangle_config.velocity));
-    auto current_pos = rect.getPosition();
-    if ((current_pos.x - 1 <= 0) || (current_pos.y - 1 <= 0)) {
-      Xv = 1.f;
-      Vy = 0.5f;
-    }
-    if ((current_pos.x + rect.getSize().x >= WINDOW_W) ||
-        (current_pos.y + rect.getSize().y >= WINDOW_H)) {
-      Xv = -1.f;
-      Vy = -0.5f;
-    }
-    // std::cout << current_pos.x << " " << current_pos.y << std::endl;
 
-    rect.move(Xv, Vy);
-    sf::Vector2f pos = rect.getPosition();
-    window.draw(rect);
+    for (auto &rect : rects) {
+      float Xv = 1.f, Vy = 0.5f;
+      auto current_pos = rect.getPosition();
+      if ((current_pos.x - 1 <= 0) || (current_pos.y - 1 <= 0)) {
+        Xv = 1.f;
+        Vy = 0.5f;
+      }
+      if ((current_pos.x + rect.getSize().x >= WINDOW_W) ||
+          (current_pos.y + rect.getSize().y >= WINDOW_H)) {
+        Xv = -1.f;
+        Vy = -0.5f;
+      }
+
+      rect.move(Xv, Vy);
+      sf::Vector2f pos = rect.getPosition();
+      window.draw(rect);
+    }
+
     window.display();
   }
 
